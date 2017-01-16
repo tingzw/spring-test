7eb7075f6ccb2bf8578693cd2a2a99a26373caa7
tingzw <hhxy_tingzw@126.com>
2017-01-16 16:20:16 +0800

    init

--- src/test/java/com/tingzw/dao/impl/UserDaoImplTest.java
+++ src/test/java/com/tingzw/dao/impl/UserDaoImplTest.java
@@ -27,7 +27,7 @@ public class UserDaoImplTest {
 
     @Test
     public void testGetUser() throws Exception {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
             User user = userDao.getUser(16);
             System.out.println(user);
         }
